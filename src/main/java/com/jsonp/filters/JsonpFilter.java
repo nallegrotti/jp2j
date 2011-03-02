fbd553cb6b89593e9f668944ae0280d289b69aef
Nicolás Allegrotti <nicolas.allegrotti@mercadolibre.com>
2011-03-02 11:56:23 -0300

    Se agrega la configuración del header que controla la activación del switch. En caso de no configurarlo el filter está activo siempre.

--- src/main/java/com/jsonp/filters/JsonpFilter.java
+++ src/main/java/com/jsonp/filters/JsonpFilter.java
@@ -15,6 +15,9 @@ import com.jsonp.wrappers.JsonpRequestWrapper;
 import com.jsonp.wrappers.JsonpResponseWrapper;
 
 public class JsonpFilter implements Filter {
+	
+	private String headerSwitchName=null;
+	private String headerSwitchValue=null;
 
 	@Override
 	public void destroy() {
@@ -47,12 +50,45 @@ public class JsonpFilter implements Filter {
 
 	public boolean isFilterActive(HttpServletRequest originalRequest,
 			HttpServletResponse originalResponse) {
-		String filterSwitch = originalRequest.getHeader("X-Public");
-		return "True".equals(filterSwitch);
+		String switchName = getHeaderSwitchName();
+		if (switchName!=null) {
+			String filterSwitch = originalRequest.getHeader(switchName);
+			String switchValue = getHeaderSwitchValue();
+			return (switchValue==null && filterSwitch!=null) 
+				|| (switchValue!=null && switchValue.equals(filterSwitch));
+		}else {
+			return true;
+		}
+	}
+
+	public void setHeaderSwitch(String headerSwitch) {
+		if (headerSwitch != null) {
+			String[] headerSwitchPair = headerSwitch.split(":");
+			headerSwitchName = headerSwitchPair.length >= 1 ? headerSwitchPair[0]
+					: null;
+			headerSwitchValue = headerSwitchPair.length >= 2 ? headerSwitchPair[1]
+					: null;
+		}		
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
+	public void init(FilterConfig conf) throws ServletException {
+		setHeaderSwitch(conf.getInitParameter("headerSwitch"));
+	}
 
+	public void setHeaderSwitchName(String headerSwitchName) {
+		this.headerSwitchName = headerSwitchName;
+	}
+
+	public String getHeaderSwitchName() {
+		return headerSwitchName;
+	}
+
+	public void setHeaderSwitchValue(String headerSwitchValue) {
+		this.headerSwitchValue = headerSwitchValue;
+	}
+
+	public String getHeaderSwitchValue() {
+		return headerSwitchValue;
 	}
 }

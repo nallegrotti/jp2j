d8dfabf69b8a263fe01da39ac449097a0bc5f65c
Nicolás Allegrotti <nicolas.allegrotti@mercadolibre.com>
2011-03-03 15:21:00 -0300

    Se eliminan dependencias innecesarias de mockapi

--- mockapi/grails-app/conf/BuildConfig.groovy
+++ mockapi/grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,6 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -13,21 +12,7 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-//	mavenRepo "http://10.10.42.47:8180/nexus/content/groups/public"
-	mavenRepo "http://git.ml.com:8081/nexus/content/groups/Arquitectura"
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-//		runtime 'com.jsonp:jp2j:0.0.1.c'
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-	//compile ':mlapi:1.0.4'
-        // runtime 'mysql:mysql-connector-java:5.1.5'
     }
 }
